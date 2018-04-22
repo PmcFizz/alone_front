b34d2696b9e4b3e8b52c2bf47fbb4361c48f7825
FizzPang <mengchen_0212@foxmail.com>
2018-04-22 22:56:06 +0800

    首页搜素列表 结果样式

---
+++ src/views/zc/login.vue
@@ -0,0 +1,15 @@
+<!--用户登陆-->
+<template>
+  <div class="zc_login">
+
+  </div>
+</template>
+<script>
+  export default{
+    name: 'zcLogin',
+    data(){
+      return {}
+    },
+    methods: {}
+  }
+</script>
---
+++ src/views/zc/register.vue
@@ -0,0 +1,15 @@
+<!--注册用户-->
+<template>
+  <div class="zc_register">
+
+  </div>
+</template>
+<script>
+  export default{
+    name: 'zcRegister',
+    data(){
+      return {}
+    },
+    methods: {}
+  }
+</script>
