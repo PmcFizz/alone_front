32ffc854b6c7eea869d5e4335fc9b236617b811c
pangmengchen <mengchen_0212@foxmail.com>
2018-04-28 17:49:26 +0800

    api.mczaiyun.top

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://115.159.52.223:3000',
+        target: 'http://api.mczaiyun.top',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
