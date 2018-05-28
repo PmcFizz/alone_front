a1711ea17e3a155103477cffbf16140479fd97d0
pangmengchen <mengchen_0212@foxmail.com>
2018-05-28 11:18:37 +0800

    公司新增接口 任职经历新增接口 评价新增接口

--- src/views/interfaceDoc/sj_interface.vue
+++ src/views/interfaceDoc/sj_interface.vue
@@ -52,6 +52,68 @@
                 },
                   "msg": "请求成功"
                 }`
+          }, {
+            interfaceName: '添加公司接口',
+            interfaceURL: '/api/company/createOne',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'name', des: '公司名', mustHave: true, remark: '无'},
+              {prototype: 'shortName', des: '公司短名', mustHave: false, remark: '无'},
+              {prototype: 'companyLink', des: '公司链接', mustHave: false, remark: '无'},
+              {prototype: 'logo', des: '公司logo地址', mustHave: false, remark: '无'},
+              {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'},
+              {prototype: 'basic', des: '融资阶段,人数,类型,地址', mustHave: false, remark: '对象Object'},
+              {prototype: 'type', des: '公司类型', mustHave: false, remark: '无'},
+              {prototype: 'companyAddress', des: '公司地址', mustHave: false, remark: '无'},
+              {prototype: 'tags', des: '标签', mustHave: false, remark: '数字Array'},
+              {prototype: 'companyIntroText', des: '公司介绍', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
+          }, {
+            interfaceName: '添加任职经历接口',
+            interfaceURL: '/api/occupationalHistory/createOne',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'companyName', des: '公司名', mustHave: true, remark: '无'},
+              {prototype: 'startTime', des: '开始任职时间', mustHave: false, remark: '时间格式为2018-05-12'},
+              {prototype: 'endTime', des: '结束任职时间', mustHave: false, remark: '无'},
+              {prototype: 'position', des: '职位', mustHave: false, remark: '无'},
+              {prototype: 'userId', des: '用户id', mustHave: false, remark: '无'},
+              {prototype: 'workDes', des: '工作描述', mustHave: false, remark: '无'},
+              {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
+          }, {
+            interfaceName: '添加公司评价接口',
+            interfaceURL: '/api/evaluate/createOne',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'content', des: '评价内容', mustHave: true, remark: '无'},
+              {prototype: 'companyId', des: '公司id', mustHave: false, remark: ''},
+              {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'},
+              {prototype: 'createDate', des: '创建时间', mustHave: false, remark: '无'},
+              {prototype: 'status', des: '状态', mustHave: false, remark: '无'},
+              {prototype: 'lastModifiedDate', des: '最后更新', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
           }
         ],
         showIndex: -1
