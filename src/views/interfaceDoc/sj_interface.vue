36d8d6a58a6a32aef4abaedcfb92e63f585650db
pangmengchen <mengchen_0212@foxmail.com>
2018-05-31 10:26:00 +0800

    查询当前登陆了人的接口

--- src/views/interfaceDoc/sj_interface.vue
+++ src/views/interfaceDoc/sj_interface.vue
@@ -76,7 +76,7 @@
                   "msg": "请求成功"
                 }`
           }, {
-            interfaceName: '添加任职经历接口',
+            interfaceName: '添加自己的任职经历接口',
             interfaceURL: '/api/occupationalHistory/createOne',
             requestWay: 'POST | JSON',
             sendData: [
@@ -84,7 +84,6 @@
               {prototype: 'startTime', des: '开始任职时间', mustHave: false, remark: '时间格式为2018-05-12'},
               {prototype: 'endTime', des: '结束任职时间', mustHave: false, remark: '无'},
               {prototype: 'position', des: '职位', mustHave: false, remark: '无'},
-              {prototype: 'userId', des: '用户id', mustHave: false, remark: '无'},
               {prototype: 'workDes', des: '工作描述', mustHave: false, remark: '无'},
               {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'}
             ],
@@ -98,6 +97,62 @@
           }, {
             interfaceName: '添加公司评价接口',
             interfaceURL: '/api/evaluate/createOne',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'content', des: '评价内容', mustHave: true, remark: '无'},
+              {prototype: 'companyId', des: '公司id', mustHave: false, remark: ''},
+              {prototype: 'createDate', des: '创建时间', mustHave: false, remark: '无'},
+              {prototype: 'status', des: '状态', mustHave: false, remark: '无'},
+              {prototype: 'lastModifiedDate', des: '最后更新', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
+          }, {
+            interfaceName: '查询当前登录人的评价',
+            interfaceURL: '/api/evaluate/queryMyEvaluate',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'content', des: '评价内容', mustHave: true, remark: '无'},
+              {prototype: 'companyId', des: '公司id', mustHave: false, remark: ''},
+              {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'},
+              {prototype: 'createDate', des: '创建时间', mustHave: false, remark: '无'},
+              {prototype: 'status', des: '状态', mustHave: false, remark: '无'},
+              {prototype: 'lastModifiedDate', des: '最后更新', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
+          }, {
+            interfaceName: '查询当前登录人的任职',
+            interfaceURL: '/api/occupationalHistory/queryMyOccup',
+            requestWay: 'POST | JSON',
+            sendData: [
+              {prototype: 'content', des: '评价内容', mustHave: true, remark: '无'},
+              {prototype: 'companyId', des: '公司id', mustHave: false, remark: ''},
+              {prototype: 'createUserId', des: '创建人id', mustHave: false, remark: '无'},
+              {prototype: 'createDate', des: '创建时间', mustHave: false, remark: '无'},
+              {prototype: 'status', des: '状态', mustHave: false, remark: '无'},
+              {prototype: 'lastModifiedDate', des: '最后更新', mustHave: false, remark: '无'}
+            ],
+            returnData: `{
+                "code": 200,
+                  "data": {
+                  "msg": "添加成功"
+                },
+                  "msg": "请求成功"
+                }`
+          }, {
+            interfaceName: '查询当前登录人的信息',
+            interfaceURL: '/api/user/queryMyInfo',
             requestWay: 'POST | JSON',
             sendData: [
               {prototype: 'content', des: '评价内容', mustHave: true, remark: '无'},
